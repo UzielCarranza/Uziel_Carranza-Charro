34ed2eca8069505d0b7b96677b27d8e5859b8665
UzielCarranza <uzielcarranzac97@gmail.com>
2022-12-27 10:09:17 -0500

    adds information component

---
+++ src/NavBar.js
@@ -0,0 +1,16 @@
+// export const NavBar = () => {
+//
+//     return (
+//         <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+//             <div>
+//                 <ul>
+//                     <li style={{color: "#ffb703"}}>Uziel Carranza</li>
+//                     <li>Projects</li>
+//                     <li>About</li>
+//                     <li>About</li>
+//                 </ul>
+//             </div>
+//         </nav>
+//     )
+//
+// }
