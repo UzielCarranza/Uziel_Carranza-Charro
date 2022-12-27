e5f06cc2c2693320184e93a05ec1df7bf0597984
UzielCarranza <uzielcarranzac97@gmail.com>
2022-12-27 18:20:37 -0500

    Adds connect with me links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {NavBar} from "./components/NavBar";
 import {Introduction} from "./components/Introduction";
+import {LinkElements} from "./components/LinkElements";
 
 function App() {
     return (
         <>
             <NavBar/>
             <Introduction/>
+            <LinkElements/>
         </>
     )
         ;
