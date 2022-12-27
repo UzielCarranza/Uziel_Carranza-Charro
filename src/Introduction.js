87875d7c1324e3b13c005779635d44d0a4bf77be
UzielCarranza <uzielcarranzac97@gmail.com>
2022-12-27 11:20:02 -0500

    adds profile picture and introduction section

--- src/Introduction.js
+++ src/Introduction.js
@@ -1,10 +1,18 @@
 import "./styles.css";
+import profile_img from "./img/profile_img.png";
 
 export const Introduction = () => {
     //TODO: Add link to get to know me directing to linkedin page
     return (
-        <div className="wrapper-about_me">
-            <h1>Hello there I am <strong className="call-attention_word">Uziel Carranza Charro</strong></h1>
+        <div className="wrapper-about_me responsive-column">
+            <div className="wrapper-photo">
+                <div className="wrapper-about_me-content  responsive-column">
+                    <h3>Hello I am </h3>
+                    <h2><strong className="call-attention_word">Uziel Carranza Charro</strong></h2>
+                </div>
+                <img className="profile_photo" src={profile_img} alt="Uziel Carranza Charro"/>
+            </div>
+
             <p>A <strong className="call-attention_word">Software Developer</strong> with a strong passion for
                 building responsive web applications get to <a href="https://www.linkedin.com/in/uziel-carranza-charro/"
                                                                target="_blank"><strong className="call-attention_word">Know
