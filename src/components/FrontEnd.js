8cc5cc17354bf50020e49b2b5320fa6e9a55beb2
UzielCarranza <uzielcarranzac97@gmail.com>
2022-12-28 18:26:34 -0500

    removes navbar component

--- src/components/FrontEnd.js
+++ src/components/FrontEnd.js
@@ -1,13 +1,11 @@
 import {developmentProjects} from "../server.js";
 import CarouselComponent from "./Carousel";
-import {NavBar} from "./NavBar";
 
 export const FrontEnd = () => {
     const frontEndProjects = developmentProjects.frontEnd;
 
     return (
         <>
-            <NavBar/>
             <CarouselComponent projects={frontEndProjects}/>
         </>)
 }
