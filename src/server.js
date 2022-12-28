dad6cbdfd8323664b4915bf4d1b189c2ea40871f
UzielCarranza <uzielcarranzac97@gmail.com>
2022-12-28 12:08:56 -0500

    Adds react router and carousel

---
+++ src/server.js
@@ -0,0 +1,74 @@
+export const developmentProjects = {
+    frontEnd:
+        [
+            {
+                id: 1,
+                title: 'bomb reporter',
+                url: 'img/bomb-reporter.png',
+                description: 'A full-stack application developed by a team of four developers focusing to give Ukrainians the tools to report bombs near their area.',
+                github_location: `https://github.com/Team-4-capstone`
+            },
+
+            {
+                id: 2,
+                title: 'GetMyWeather',
+                url: 'img/weather.png',
+                description: 'A web application developed with BootStrap, vanilla javaScript, CSS3, and HTML5',
+                github_location: 'https://github.com/UzielCarranza/weather-app'
+            },
+            {
+                id: 2,
+                title: 'Pokemon Lister ',
+                url: 'img/pokemons-lister.png',
+                description: 'A React web application that displays Pokemons in card format. Some of the features include pagination, sorting options, etc...',
+                github_location: 'https://github.com/UzielCarranza/pokemon-lister'
+            },
+
+            {
+                id: 3,
+                title: 'Coffee Project',
+                url: 'img/coffee.png',
+                description: 'A pair programming project developed with vanilla javaScript, CSS3, and HTML5',
+                github_location: 'https://uziel-jordan.github.io/coffee-project-1/'
+            },
+        ],
+    backEnd: [
+        {
+            id: 1,
+            title: 'Microservice API Music Store Catalog',
+            url: 'img/music-store-catalog.png',
+            description: 'A microservice with a REST API that provides CRUD functionality for a database hosted and deployed on Heroku using Docker to containerize the application.',
+            github_location: 'https://github.com/UzielCarranza/music-store-catalog'
+        },
+        {
+            id: 2,
+            title: 'Microservice API Music Store Recommendations',
+            url: 'img/music_store_recommendation.png',
+            description: 'A microservice with a REST API that provides CRUD functionality for a database hosted and deployed on Heroku using Docker to containerize the application.',
+            github_location: 'https://github.com/UzielCarranza/music-store-recommendation'
+        },
+        {
+            id: 3,
+            title: 'Contacts Manager App',
+            url: 'img/contactsManager.png',
+            description: 'A pair programming project developed with vanilla javaScript, CSS3, and HTML5',
+            github_location: 'https://github.com/Uziel-Daniel/contacts-manager'
+        }, {
+            id: 4,
+            title: 'Hangman Game',
+            url: 'img/hangman.png',
+            description: 'One of the populars games, the hangman game developed using Java',
+            github_location: 'https://github.com/UzielCarranza/hangman-project'
+        }
+    ],
+    fullStack: [
+        {
+            id: 1,
+            title: 'Underground Movies Madness',
+            url: 'img/moviesApp.png',
+            description: 'A pair programming project developed with vanilla javaScript, CSS3, and HTML5',
+            github_location: 'https://github.com/uziel-tyreik/movies'
+        }
+    ]
+
+}
