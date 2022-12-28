375c6ebd7e1a35112fc2f0e9537fd7be3566ade7
UzielCarranza <uzielcarranzac97@gmail.com>
2022-12-28 18:27:13 -0500

    renders the component at thte root index

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import {BrowserRouter} from "react-router-dom";
 import Pages from "./Pages";
+import {NavBar} from "./components/NavBar";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
         <BrowserRouter>
+            <NavBar/>
             <Pages/>
         </BrowserRouter>
 );
