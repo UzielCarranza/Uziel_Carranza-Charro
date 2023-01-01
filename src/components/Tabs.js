b678dd42dd2a786b89e4d2cd5602c9cd6983d224
UzielCarranza <uzielcarranzac97@gmail.com>
2023-01-01 15:55:32 -0500

    solves issue preventing deployment to netlify

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -11,7 +11,8 @@ export const Tabs = (props) => {
     const active = 'inline-block bg-gray-100 text-blue-600 rounded-t-lg py-4 px-4 text-sm font-medium text-center active dark:bg-gray-800 text-black';
     const inactive = 'inline-block text-white hover:text-gray-600 hover:bg-gray-50 rounded-t-lg py-4 px-4 text-sm font-medium text-center dark:text-gray-400  dark:hover:bg-gray-800 dark:hover:text-gray-300'
     return (
-        <a href="#"
+        <a href="/"
+           onClick={(e) => e.preventDefault()}
            className={props.display ? active : inactive}>
             {selectedCategory}
         </a>
