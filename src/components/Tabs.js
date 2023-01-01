37310d8752d4b6d6d3fbdcf5e3a1ae5c3bdca734
UzielCarranza <uzielcarranzac97@gmail.com>
2023-01-01 15:14:22 -0500

    adds a component that displays tabs section

---
+++ src/components/Tabs.js
@@ -0,0 +1,20 @@
+import "../styles/technologies.css";
+import {useEffect, useState} from "react";
+
+export const Tabs = (props) => {
+    const [selectedCategory, setSelectedCategory] = useState(true);
+
+    useEffect(() => {
+        props.category ? setSelectedCategory(props.category) : setSelectedCategory(' ');
+    }, [props.category])
+
+    const active = 'inline-block bg-gray-100 text-blue-600 rounded-t-lg py-4 px-4 text-sm font-medium text-center active dark:bg-gray-800 text-black';
+    const inactive = 'inline-block text-white hover:text-gray-600 hover:bg-gray-50 rounded-t-lg py-4 px-4 text-sm font-medium text-center dark:text-gray-400  dark:hover:bg-gray-800 dark:hover:text-gray-300'
+    return (
+        <a href="#"
+           className={props.display ? active : inactive}>
+            {selectedCategory}
+        </a>
+
+    )
+}
